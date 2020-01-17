2a7558e0328e77c581877e9deda31c1ee649cf03
daniel <menacanaviridaniel@gmail.com>
2020-01-17 00:22:35 -0400

    CRUD con PHP 7 y POO, y archivo de conexión

---
+++ 11_CRUD/conexion.php
@@ -0,0 +1,28 @@
+<?php 
+	class Conectar{
+		private $servidor = "localhost";
+		private $usuario = "root";
+		private $bd = "crudpoo";
+		private $password = "";
+
+		public function conexion(){
+			$conexion = mysqli_connect(
+				$this->servidor,
+				$this->usuario,
+				$this->password,
+				$this->bd
+			);
+			return $conexion;
+		}
+	}
+
+	// Codigo para verificar si existe conexión 
+	/*
+	$obj = new Conectar();
+	if($obj->conexion()){
+		echo 'Conectado con exito';
+	}else{
+		echo 'Conexion sin exito';
+	}
+	*/
+ ?>
